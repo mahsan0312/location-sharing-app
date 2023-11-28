0f5891133b9c4cfb8c30dffe869e642db0c857e8
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-28 22:38:03 +0500

    Adding Models and Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+  get 'trips/create'
+  get 'trips/show'
+  get 'checkins/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
