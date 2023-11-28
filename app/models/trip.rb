ad44ed688466a433b6c85b88951bf6c699e0d19e
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-28 22:39:08 +0500

    Adding Trip Model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,18 @@
+# app/models/trip.rb
 class Trip < ApplicationRecord
+  before_create :set_uuid
+  has_many :checkins # trip model's association with the checkins model
+
+  # a method that creates a random uuid for each trip before its created
+  def set_uuid
+    self.uuid = SecureRandom.uuid
+  end
+
+  # a method that generates a custom JSON output for our trip objects
+  def as_json(options={})
+    super(
+      only: [:id, :name, :uuid],
+      include: { checkins: { only: [:lat, :lng, :trip_id] } }
+    )
+  end
 end
