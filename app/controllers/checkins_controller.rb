18877e03eac16a6191bded6271da442cdbfad1a0
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-29 16:19:26 +0500

    Assets Precompile Issue

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,4 +1,13 @@
+# app/controllers/checkins_controller.rb
+
 class CheckinsController < ApplicationController
   def create
+    @checkin = Checkin.new(checkin_params)
+    render json: @checkin.as_json(only: [:lat, :lng, :trip_id]) if @checkin.save
   end
+
+  private
+    def checkin_params
+      params.permit(:trip_id, :lat, :lng)
+    end
 end
